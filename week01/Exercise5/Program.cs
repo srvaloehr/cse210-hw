a132a33308a34f82a063ec23e6d1ec1610e46e60
srvaloehr <srvaloehr@gmail.com>
2025-07-13 14:44:15 -0700

    sync

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        ShowWelcome();
+
+        string name = GetName();
+        int number = GetNumber();
+
+        int square = Square(number);
+
+        ShowResult(name, square);
+    }
+
+    static void ShowWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int Square(int number)
+    {
+        return number * number;
+    }
+
+    static void ShowResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
