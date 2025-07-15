ecf678043cffba80561b770e2c4ed5c72b7b5d40
srvaloehr <srvaloehr@gmail.com>
2025-07-14 22:21:35 -0700

    abstract

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Project Manager";
+        job1._company = "Reign Construction";
+        job1._startYear = 2025;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Caster";
+        job2._company = "CMC";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Stephen";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
